f69ec61cb70cf207d54af2704c7397a6db204b85
Lina Jones <lina.jones630@gmail.com>
2019-03-19 19:15:16 -0600

    (feat) start graphQL server setup

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,11 @@
+//file connects to remote prisma db and lets us query via JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+//user express middleware to handle cookies & populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, deets => {
+  console.log(`server now running on http://localhost:${deets.port}`);
+});
