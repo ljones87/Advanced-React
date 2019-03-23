2333c70b9e548c750f98516c3c625e757ed4a4e0
Lina Jones <lina.jones630@gmail.com>
2019-03-22 21:05:49 -0600

    (feat) can upload image and on submit post to server

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -41,6 +41,24 @@ export default class CreateItem extends Component {
     this.setState({ [name]: val })
   }
 
+   uploadFile = async (e) => {
+    console.log('..uploading file');
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+    const res = await fetch(process.env.CLOUD_URL, {
+      method: 'POST',
+      body: data
+    })
+    const file = await res.json()
+    console.log(file)
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
+  }
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -55,6 +73,17 @@ export default class CreateItem extends Component {
           }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="title">File
+              <input
+                type="file"
+                id="file"
+                name="file"
+                placeholder="Upload File"
+                required
+                onChange={this.uploadFile}
+              />
+              {this.state.image && <img src={this.state.image} alt="upload preview" /> }
+            </label>
               <label htmlFor="title">Title
                 <input
                   type="text"
