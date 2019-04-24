5e8009d6c5e80eac5665d64ebcc3ec03453450b1
ljones <lina.jones630@gmail.com>
2019-04-24 09:09:49 -0600

    (feat) component refactored to integrate graphQL mutation and formik

--- sick-fits/frontend/components/Signup.js
+++ sick-fits/frontend/components/Signup.js
@@ -1,21 +1,48 @@
 import React, { Component, useState } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { graphql, compose } from "react-apollo";
 import { withFormik, Formik, Field, Form as FormikForm } from "formik";
 
 import StyledForm from "./styles/StyledForm";
 import Error from "./ErrorMessage";
 
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    signup(email: $email, name: $name, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const handleSubmit = (values, { props, resetForm, setErrors, setSubmitting }) => {
+  props.mutate({ variables: values }).then(() => {
+    resetForm();
+  })
+  .catch(err => {
+      setErrors(err)
+      setSubmitting(false)
+  })
+};
+
 let Signup = props => {
   const email = "email";
   const password = "password";
   const name = "name";
+  const { isSubmitting, errors } = props;
 
   return (
     <StyledForm>
-      <FormikForm>
-        <fieldset>
+      <FormikForm method="post" onSubmit={props.handleSubmit}>
+        <fieldset disabled={isSubmitting} aria-busy={isSubmitting}>
           <h2>Sign up for account</h2>
+          <Error error={errors} />
           <label>Email</label>
           <Field type={email} name={email} placeholder="Email" />
           <label>Name</label>
@@ -29,19 +56,18 @@ let Signup = props => {
   );
 };
 
-Signup = withFormik({
-  mapPropsToValues({ email, password, name }) {
+Signup = compose(
+  graphql(SIGNUP_MUTATION),
+  withFormik({
+    mapPropsToValues({ email, password, name }) {
       return {
-        email: email || '',
-        password: password || '',
-        name: name || ''
-      }
-  },
-  handleSubmit(values, { resetForm, setErrors }) {
-    console.log(values);
-    resetForm();
-    setErrors();
-  }
-})(Signup);
+        email: email || "",
+        password: password || "",
+        name: name || ""
+      };
+    },
+    handleSubmit
+  })
+)(Signup);
 
 export default Signup;
